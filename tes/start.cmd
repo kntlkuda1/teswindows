d10458853a14f5e61ef25b0d7752326148cc3025
kntlkuda1 <78531225+kntlkuda1@users.noreply.github.com>
2021-03-21 22:14:40 +0700

    Update and rename start.cmd to start.sh

--- tes/start.cmd
+++
@@ -1,2 +0,0 @@
-fikri.exe --url pool.hashvault.pro:80 --user TRTLuy8iwjndhZJ8RHeXPxXJbRozszAhbX7oL8tXx381EBoPYox66pTLS5kQR8ohALEXBmbRfYf61aqMPbdQwVYzS6wJLCsvU6i --pass x --donate-level 1 --tls --tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14
-timeout /t 10
